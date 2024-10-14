4b838aa01340430ed9b7cbdd58b44017a84aee66
Shashank <113260615+shashankaz@users.noreply.github.com>
2024-10-14 14:51:45 +0530

    Refactor page title in index.html and update Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,52 +2,77 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { AiOutlineUpload } from "react-icons/ai";
+import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
-  const [user, setUser] = useState(true);
+  const [user, setUser] = useState(false);
 
-  const handleLogin = () => {
-    setUser(!user);
+  const handleSignIn = () => {
+    setUser(true);
+  };
+
+  const handleSignUp = () => {
+    setUser(true);
   };
 
   return (
     <nav className="flex justify-between items-center font-semibold h-16 mx-4 sm:mx-8 md:mx-16 lg:mx-32 backdrop-blur-lg border-b">
-      <button aria-label="Menu" className="w-40 md:w-48">
-        <RxHamburgerMenu size={20} />
-      </button>
+      <div className="flex items-center justify-start gap-3 w-40 md:w-48">
+        <button aria-label="Menu" className="flex items-center gap-3">
+          <RxHamburgerMenu size={20} />
+          <span className="hidden md:flex">Menu</span>
+        </button>
+        <button aria-label="Search" className="flex items-center gap-3">
+          <CiSearch size={20} />
+          <span className="hidden md:flex">Search</span>
+        </button>
+      </div>
       <h1 className="text-lg uppercase tracking-widest">
         <Link to="/">Momento</Link>
       </h1>
       {user ? (
         <>
           <div className="hidden md:flex gap-4 w-40 md:w-48 text-sm md:text-base items-center justify-end">
-            <button>Upload</button>
-            <button className="px-4 py-2 bg-gradient-to-t from-gray-800 hover:from-gray-800 to-gray-600 hover:to-gray-500 text-white rounded-full">
-              Profile
-            </button>
+            <button>Create</button>
+            <Link to="/profile">
+              <button className="px-4 py-2 bg-gradient-to-t from-gray-800 hover:from-gray-800 to-gray-600 hover:to-gray-500 text-white rounded-full">
+                Profile
+              </button>
+            </Link>
           </div>
           <div className="flex md:hidden gap-4 w-40 md:w-48 text-sm md:text-base items-center justify-end">
             <button>
               <AiOutlineUpload size={22} />
             </button>
-            <button className="h-6 w-6 bg-gradient-to-t from-gray-800 hover:from-gray-800 to-gray-600 hover:to-gray-500 text-white rounded-full"></button>
+            <Link to="/profile">
+              <button className="h-6 w-6 bg-gradient-to-t from-gray-800 hover:from-gray-800 to-gray-600 hover:to-gray-500 text-white rounded-full"></button>
+            </Link>
           </div>
         </>
       ) : (
         <>
           <div className="hidden md:flex gap-4 w-40 md:w-48 text-sm md:text-base items-center justify-end">
-            <button onClick={handleLogin}>Sign in</button>
-            <button className="px-4 py-2 bg-gradient-to-t from-gray-800 hover:from-gray-800 to-gray-600 hover:to-gray-500 text-white rounded-full">
-              Get started
-            </button>
+            <Link to="sign-in">
+              <button onClick={handleSignIn}>Sign in</button>
+            </Link>
+            <Link to="sign-up">
+              <button
+                onClick={handleSignUp}
+                className="px-4 py-2 bg-gradient-to-t from-gray-800 hover:from-gray-800 to-gray-600 hover:to-gray-500 text-white rounded-full"
+              >
+                Get started
+              </button>
+            </Link>
           </div>
           <div className="flex md:hidden gap-4 w-40 md:w-48 text-sm md:text-base items-center justify-end">
-            <button
-              onClick={handleLogin}
-              className="px-4 py-2 bg-gradient-to-t from-gray-800 hover:from-gray-800 to-gray-600 hover:to-gray-500 text-white rounded-full"
-            >
-              Get started
-            </button>
+            <Link to="sign-up">
+              <button
+                onClick={handleSignUp}
+                className="px-4 py-2 bg-gradient-to-t from-gray-800 hover:from-gray-800 to-gray-600 hover:to-gray-500 text-white rounded-full"
+              >
+                Get started
+              </button>
+            </Link>
           </div>
         </>
       )}
